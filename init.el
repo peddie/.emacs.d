c10c767f79d31ebd6c101a7d65dde6f0f27b5885
Matt Peddie <matt.peddie@swift-nav.com>
2022-10-17 09:35:30 +1000

    ido mode is not coming back

--- init.el
+++ init.el
@@ -216,32 +216,6 @@
    ("C-<" . #'mc/mark-previous-like-this)
    ("C-c C-<" . #'mc/mark-all-like-this)))
 
-;; (use-package ido
-;;   :config
-;;   (setq 
-;;    ido-save-directory-list-file (concat user-emacs-directory "cache/ido.last")
-;;    ido-ignore-files '("GTAGS" "GSYMS" "GRTAGS" "GPATH" "TAGS")
-;;    ido-ignore-buffers ;; ignore these guys
-;;    '("\\` " "^\*Mess" "^\*Back" ".*Completion" "^\*Ido" "^\*trace"
-;;      "^\*compilation" "^\*GTAGS" "^session\.*" "^\*")
-;;    ido-work-directory-list '("~/" "~/programming" "~/writing" "~/math")
-;;    ido-file-extensions-order '(".hs" ".c" ".cc" ".cpp" ".cabal" ".mk" ".py" ".el" ".sh")
-;;    ido-case-fold  t ; be case-insensitive
-;;    ido-enable-last-directory-history t ; remember last used dirs
-;;    ido-max-work-directory-list 30 ; should be enough
-;;    ido-max-work-file-list      50 ; remember many
-;;    ido-use-filename-at-point nil ; don't use filename at point (annoying)
-;;    ido-use-url-at-point nil ; don't use url at point (annoying)
-;;    ido-enable-flex-matching t ; don't try to be too smart
-;;    ido-max-prospects 10 ; don't spam my minibuffer
-;;    ido-ignore-extensions t ; ignore object files
-;;    ido-confirm-unique-completion t)
-;;   (setq confirm-nonexistent-file-or-buffer nil)
-;;   (ido-mode 'both)
-;;   :hook (ido-minibuffer-setup .
-;;                               (lambda () (make-local-variable 'resize-minibuffer-window-max-height)
-;;                                 (setq resize-minibuffer-window-max-height 1))))
-
 ;;; Input, commands, completion etc.
 
 (use-package selectrum
