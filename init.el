0e4cbdf265fd029f237bd873c62a72cbbd6446aa
Matt Peddie <matt.peddie@swift-nav.com>
2022-10-17 10:08:14 +1000

    Suppress native-comp warnings buffer popup

--- init.el
+++ init.el
@@ -93,7 +93,8 @@
           '((frame-title-format . "emacs - %b")
             (menu-bar-mode . -1)
             (scroll-bar-mode . -1)
-            (tool-bar-mode . -1)))))
+            (tool-bar-mode . -1)))
+    (setq native-comp-async-report-warnings-errors nil)))
 
 (reasonable-settings)
 
